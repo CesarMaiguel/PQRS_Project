a64481ead55e2c08f8619ad14abd17f2d4e1230a
Unknown <lucianoadb@gmail.com>
2018-05-25 18:35:58 -0500

    Vista principal en localhosto:3000/todo/

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :todos
+  get 'todo/index'
+  get 'todo/show'
+  get 'todo/create'
+  get 'todo/edit'
+  get 'todo/update'
+  get 'todo/destroy'
+  get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
